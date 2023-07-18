4186929a64b0bde4fa9600b273d7ede6024d8fda
imshivang03 <shivang.cse.nitnagaland@gmail.com>
2023-07-18 17:04:34 +0530

    first commit

---
+++ code_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor= ConsoleColor.Black;
+            Console.BackgroundColor= ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Hello World");
+        }
+    }
+}
