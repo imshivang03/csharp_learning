cea3e2c73e2bc39bd69369ff71a940e5d2ff0a3b
imshivang03 <shivang.cse.nitnagaland@gmail.com>
2023-07-18 17:28:38 +0530

    first commit

--- 28_structs/Program.cs
+++ 28_structs/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public struct Customer
+{
+    private int _id;
+    private string _name;
+
+    public int ID
+    {
+        get{ return this._id;}
+        set{ this._id= value;}
+    }
+
+    public string Name
+    {
+        get{ return this._name;}
+        set{ _name= value;}
+    }
+
+    public Customer(int Id, string Name)
+    {
+        this._id= Id;
+        this._name= Name;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("Id = {0} && Name = {1}", this._id, this._name);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer(101, "Shivang");
+        C1.PrintDetails();
+
+        Customer C2 = new Customer();
+        C2.ID = 102;
+        C2.Name = "Yashaswi";
+        C2.PrintDetails();
+
+        Customer C3= new Customer
+        {
+            ID = 103,
+            Name = "Amit"
+        };
+        C3.PrintDetails();
+    }
+}
